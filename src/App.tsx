aa935d121d2ef662cf5f2ea053d0884c1a5f634d
fandresena <fandresenaantokiniaina1@gmail.com>
2024-06-27 10:12:14 +0200

    git commit -m "Setup project with Hello, world! example"

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-8xl font-bold text-center mt-10">Hello world!</h1>
+      <h1 className="text-8xl font-bold text-center mt-10">Hello world !</h1>
     </>
   );
 }
